ccb6f55176950d6551add8ac3435307efe23d8b6
Mansour <benmedmansour@gmail.com>
2020-04-14 18:58:17 +0200

    update environnement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  data_url_old: 'https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats',
+    data_host_old:'covid-19-coronavirus-statistics.p.rapidapi.com',
+    data_api_key_old:'17e8efc68cmsh25cb37a6dd0ad33p188d8bjsn153da8654eb7',
+    world_stats_url_old: 'https://corona.lmao.ninja/all',
+    news_url:
+      'https://newsapi.org/v2/everything?q=COVID&from=2020-03-16&sortBy=publishedAt&apiKey=7f3341405a794a92a026e79902e89593&pageSize=100&language=en',
+      data_url: 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php',
+      data_host:'coronavirus-monitor.p.rapidapi.com',
+      data_api_key:'17e8efc68cmsh25cb37a6dd0ad33p188d8bjsn153da8654eb7',
+      world_stats_url: 'https://corona.lmao.ninja/all',
 };
